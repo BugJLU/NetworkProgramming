022effae5a0c25b090acfcea43d0011c8d8ee2d0
Ownsky <wangguoyu7777@me.com>
2018-04-15 09:39:36 +0800

    add socket tests

---
+++ SocketTest.cpp
@@ -0,0 +1,26 @@
+//
+// Created by mac on 2018/4/15.
+//
+
+#include <iostream>
+#include "ServerSocket.h"
+
+using namespace std;
+
+int main() {
+    cout<<"SocketTest:"<<endl;
+
+    InetAddr addr;
+    addr.setIP("127.0.0.1");
+    addr.setPort(12343);
+    Socket clientSocket;
+    clientSocket.connect(addr);
+    string data = "hello, world";
+    clientSocket.send(data.data(), data.length());
+    char data1[255];
+    clientSocket.recv(data1, 255);
+    cout<<data1<<endl;
+    clientSocket.close();
+
+    return 0;
+}
