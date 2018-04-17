5e6e0fbac2bb8cf6391870b1c68f1a0e82a6edae
leon <1578644088@qq.com>
2018-04-18 00:15:03 +0800

    FTP begin

---
+++ FTP/FTPServer/Server.cpp
@@ -0,0 +1,42 @@
+//
+// Created by leon on 18-4-18.
+//
+
+#include <iostream>
+#include "../../Multiplexing/MultiSocket.h"
+#include "../../Socket/InetAddr.h"
+#include "../../Socket/ServerSocket.h"
+#include "../../Socket/Socket.h"
+#include "../../Multiplexing/MultiSocket.h"
+#include <map>
+#include "../../FileOperation/File.h"
+
+using namespace std;
+
+
+pthread_mutex_t commandMutex;
+pthread_mutex_t dataMutex;
+pthread_mutex_t mapMutex;
+
+MultiSocket commandMulti;
+MultiSocket dataMulti;
+map<Socket, File> sockMap;
+
+void* commandThread(void*);
+void* dataThread(void*);
+
+
+int main()
+{
+    return 0;
+}
+
+void* commandThread(void* arg)
+{
+
+}
+
+void* dataThread(void* arg)
+{
+
+}
