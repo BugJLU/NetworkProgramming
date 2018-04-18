3b73288d820e628427af6585f72578b5bb1398c4
activemy <mayue_gg@163.com>
2018-04-18 19:59:39 +0800

    Merge branch 'master' of github.com:BugJLU/NetworkProgramming
    # Conflicts:
    #	CMakeLists.txt

--- FTP/FTPClient/ClientFileRecvSocket.cpp
+++ FTP/FTPClient/ClientFileRecvSocket.cpp
@@ -10,22 +10,24 @@ ClientFileRecvSocket::ClientFileRecvSocket() {
 }
 
 ClientFileRecvSocket::ClientFileRecvSocket(Info *ci) {
+
+    /*set nesssary information*/
     cInfo.qMutex = ci->qMutex;
     cInfo.fsQueue = ci->fsQueue;
     cInfo.serverSocket = ci->serverSocket;
-
+    /*all in one*/
     execute();
 }
 
 int ClientFileRecvSocket::execute() {
-
+    /*read file information from serverSocket*/
     setFileInfo();
-
+    /*read file from serverSocket and write*/
     char buffer[BUFFER_LENGTH];
     uint32_t recv_len = 0;
     File ofile;
     int openflag = ofile.open(fInfo.fileName,FILE_OUT);
-    //TODO error
+    //TODO provide error information
     if(openflag == 0) {
         while (recv_len < fInfo.fileLen) {
             uint32_t cur_len = cInfo.serverSocket.recv(buffer, BUFFER_LENGTH);
@@ -34,7 +36,7 @@ int ClientFileRecvSocket::execute() {
         }
         ofile.close();
     }
-
+    /*set status to this*/
     if(recv_len == fInfo.fileLen){
         setFileStatus(0);
     }else if(recv_len<fInfo.fileLen){
@@ -42,26 +44,26 @@ int ClientFileRecvSocket::execute() {
     }else if(recv_len>fInfo.fileLen){
         setFileStatus(-2);
     }
-
+    /*set status to queue*/
     setQueue();
-
+    /*close socket*/
     cInfo.serverSocket.close();
 
     return 0;
 }
 
 int ClientFileRecvSocket::setFileInfo() {
-
+    /*read mode,id,fileNameLength*/
     char buffer[3];
     cInfo.serverSocket.recv(buffer,3);
     fInfo.mode = buffer[0];
     fInfo.ID = buffer[1];
     fInfo.fileNameLen = buffer[2];
-
+    /*read file name*/
     char fname[fInfo.fileNameLen];
     cInfo.serverSocket.recv(fname,fInfo.fileNameLen);
     fInfo.fileName = fname;
-
+    /*read file length*/
     char flen[4];
     cInfo.serverSocket.recv(flen,4);
     uint32_t l = 0;
@@ -95,7 +97,9 @@ int ClientFileRecvSocket::setQueue() {
 
 
 void* ClientFileRecv(void* arg){
+    /*use this*/
     Info *createInfo = (Info*)arg;
     ClientFileRecvSocket cfrs(createInfo);
+    //TODO what to return
     return NULL;
 }
