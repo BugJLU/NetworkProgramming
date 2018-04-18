409550b980540fac4a28da85f4922f2d6b5ab28d
activemy <mayue_gg@163.com>
2018-04-18 19:49:42 +0800

    Merge branch 'master' of github.com:BugJLU/NetworkProgramming
    # Conflicts:
    #	CMakeLists.txt

--- FTP/FTPClient/ClientFileRecvSocket.cpp
+++ FTP/FTPClient/ClientFileRecvSocket.cpp
@@ -4,56 +4,98 @@
 
 #include "ClientFileRecvSocket.h"
 
-ClientFileRecvSocket::ClientFileRecvSocket() {}
 
-ClientFileRecvSocket::ClientFileRecvSocket(Socket s) {
-    this->sSocket = s;
+ClientFileRecvSocket::ClientFileRecvSocket() {
+
+}
+
+ClientFileRecvSocket::ClientFileRecvSocket(Info *ci) {
+    cInfo.qMutex = ci->qMutex;
+    cInfo.fsQueue = ci->fsQueue;
+    cInfo.serverSocket = ci->serverSocket;
+
     execute();
 }
 
 int ClientFileRecvSocket::execute() {
+
     setFileInfo();
-    char buffer[256];
-    //TODO
-    sSocket.recv(buffer,fInfo.fileLen);
-    //TODO fileoperation
+
+    char buffer[BUFFER_LENGTH];
+    uint32_t recv_len = 0;
+    File ofile;
+    int openflag = ofile.open(fInfo.fileName,FILE_OUT);
+    //TODO error
+    if(openflag == 0) {
+        while (recv_len < fInfo.fileLen) {
+            uint32_t cur_len = cInfo.serverSocket.recv(buffer, BUFFER_LENGTH);
+            ofile.write(buffer,cur_len);
+            recv_len = recv_len + cur_len;
+        }
+        ofile.close();
+    }
+
+    if(recv_len == fInfo.fileLen){
+        setFileStatus(0);
+    }else if(recv_len<fInfo.fileLen){
+        setFileStatus(-1);
+    }else if(recv_len>fInfo.fileLen){
+        setFileStatus(-2);
+    }
+
+    setQueue();
+
+    cInfo.serverSocket.close();
+
     return 0;
 }
 
 int ClientFileRecvSocket::setFileInfo() {
 
     char buffer[3];
-    sSocket.recv(buffer,3);
-    char m = buffer[0];
-    char i = buffer[1];
-    char fnl = buffer[2];
-    char fname[fnl];
-    sSocket.recv(fname,fnl);
-    char flen[4];
-    sSocket.recv(flen,4);
+    cInfo.serverSocket.recv(buffer,3);
+    fInfo.mode = buffer[0];
+    fInfo.ID = buffer[1];
+    fInfo.fileNameLen = buffer[2];
 
-    fInfo.mode = m;
-    fInfo.ID = i;
-    fInfo.fileNameLen = fnl;
+    char fname[fInfo.fileNameLen];
+    cInfo.serverSocket.recv(fname,fInfo.fileNameLen);
     fInfo.fileName = fname;
-    //TODO
-    fInfo.fileLen = 0;
+
+    char flen[4];
+    cInfo.serverSocket.recv(flen,4);
+    uint32_t l = 0;
+
+    for (int i = 0; i < 4; ++i) {
+        l = l<<8 + flen[i];
+    }
+
+    fInfo.fileLen = l;
 
     return 0;
 }
 
-FileInfo ClientFileRecvSocket::getFileInfo() {
-    return fInfo;
-}
 
-int ClientFileRecvSocket::setFileStatus() {
+
+int ClientFileRecvSocket::setFileStatus(int s) {
     fStatus.ID = fInfo.ID;
-    //TODO
-    fStatus.status = false;
+    fStatus.fileName = fInfo.fileName;
+    fStatus.status = s;
     return 0;
 }
 
-FileStatus ClientFileRecvSocket::getFileStatus() {
-    return fStatus;
+
+
+int ClientFileRecvSocket::setQueue() {
+    pthread_mutex_lock(cInfo.qMutex);
+    cInfo.fsQueue->push_back(fStatus);
+    pthread_mutex_unlock(cInfo.qMutex);
+    return 0;
 }
 
+
+void* ClientFileRecv(void* arg){
+    Info *createInfo = (Info*)arg;
+    ClientFileRecvSocket cfrs(createInfo);
+    return NULL;
+}
